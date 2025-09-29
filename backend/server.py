ce5badd1f9f13a9f40ece286b2dd9533168d0f4d
kolashankar <kolashankar113@gmail.com>
2025-09-29 10:37:13 +0530

    changes made

--- backend/server.py
+++ backend/server.py
@@ -110,8 +110,4 @@ async def shutdown_event():
 
 if __name__ == "__main__":
     import uvicorn
-<<<<<<< HEAD
-    uvicorn.run(app, host="0.0.0.0", port=8001)
-=======
     uvicorn.run(app, host="0.0.0.0", port=8000)
->>>>>>> 54e658b (changes made)
